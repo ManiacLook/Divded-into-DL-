1e3edec561b4bc8d58af30889d61d29a8bf5e3ce
可 路 <1300876003@qq.com>
2021-12-22 20:17:35 +0800

    13.8~13.10

--- d2lzh_pytorch.py
+++ d2lzh_pytorch.py
@@ -20,6 +20,10 @@ import zipfile
 import hashlib
 import pandas as pd
 
+def get_dataloader_workers(): 
+    """在非Windows的平台上，使用4个进程来读取数据"""
+    return 0 if sys.platform.startswith('win') else 4
+
 # 用于设置由matplotlib生成图表的轴的属性。
 def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
     """设置matplotlib的轴"""
@@ -607,7 +611,7 @@ def multibox_detection(cls_probs, offset_preds, anchors, nms_threshold=0.5, pos_
     return torch.stack(out)
 
 
-############################################ 9.6 ###########################################
+############################################ 13.6 ###########################################
 
 DATA_HUB = dict()
 DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
@@ -739,3 +743,98 @@ def load_data_pikachu(batch_size, edge_size=256, data_dir = '../Datasets/pikachu
     val_iter = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size,
                                            shuffle=False, num_workers=0)
     return train_iter, val_iter
+
+############################################ 13.9 ###########################################
+# DATA_HUB['voc2012'] = (DATA_URL + 'VOCtrainval_11-May-2012.tar',
+#                            '4e443f8a2eca6b1dac8a6c57641b67dd40621a49')
+
+# voc_dir = download_extract('voc2012', 'VOCdevkit/VOC2012')
+
+def read_voc_images(voc_dir, is_train=True):
+    """读取所有VOC图像并标注"""
+    txt_fname = os.path.join(voc_dir, 'ImageSets', 'Segmentation', 'train.txt' if is_train else 'val.txt')
+    mode = torchvision.io.image.ImageReadMode.RGB
+    with open(txt_fname, 'r') as f:
+        images = f.read().split()
+    features, labels = [], []
+    for i, fname in enumerate(images):
+        features.append(torchvision.io.read_image(os.path.join(voc_dir, 'JPEGImages', f'{fname}.jpg')))
+        labels.append(torchvision.io.read_image(os.path.join(voc_dir, 'SegmentationClass', f'{fname}.png'), mode))
+    return features, labels
+
+
+VOC_COLORMAP = [[0, 0, 0], [128, 0, 0], [0, 128, 0], [128, 128, 0],
+                [0, 0, 128], [128, 0, 128], [0, 128, 128], [128, 128, 128],
+                [64, 0, 0], [192, 0, 0], [64, 128, 0], [192, 128, 0],
+                [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
+                [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
+                [0, 64, 128]]
+
+VOC_CLASSES = ['background', 'aeroplane', 'bicycle', 'bird', 'boat',
+               'bottle', 'bus', 'car', 'cat', 'chair', 'cow',
+               'diningtable', 'dog', 'horse', 'motorbike', 'person',
+               'potted plant', 'sheep', 'sofa', 'train', 'tv/monitor']
+
+def voc_colormap2label():
+    """构建从RGB到VOC类别索引的映射"""
+    colormap2label = torch.zeros(256 ** 3, dtype=torch.long)
+    # 用RGB像素来计算索引idx，将类别序号赋给idx对应的值
+    for i, colormap in enumerate(VOC_COLORMAP):  
+        colormap2label[(colormap[0] * 256 + colormap[1]) * 256 + colormap[2]] = i
+    return colormap2label
+
+
+def voc_label_indices(colormap, colormap2label):
+    """将VOC标签中的RGB值映射到它们的类别索引"""
+    colormap = colormap.permute(1, 2, 0).numpy().astype('int32')
+    # 用RGB像素来获得对应索引idx
+    idx = ((colormap[:, :, 0] * 256 + colormap[:, :, 1]) * 256 + colormap[:, :, 2])  
+    return colormap2label[idx]
+
+
+def voc_rand_crop(feature, label, height, width):
+    """随机裁剪特征和标签图像"""
+    rect = torchvision.transforms.RandomCrop.get_params(feature, (height, width))
+    feature = torchvision.transforms.functional.crop(feature, *rect)
+    label = torchvision.transforms.functional.crop(label, *rect)
+    return feature, label
+
+
+class VOCSegDataset(torch.utils.data.Dataset):
+    """一个用于加载VOC数据集的自定义数据集"""
+    
+    def __init__(self, is_train, crop_size, voc_dir):
+        # output[channel] = (input[channel] - mean[channel]) / std[channel]
+        self.transform = torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.crop_size = crop_size
+        features, labels = read_voc_images(voc_dir, is_train=is_train)
+        self.features = [self.normalize_image(feature) for feature in self.filter(features)]
+        self.labels = self.filter(labels)
+        self.colormap2label = voc_colormap2label()
+        print('read ' + str(len(self.features)) + ' examples')
+    
+    # 像素值标准化
+    def normalize_image(self, img):
+        return self.transform(img.float() / 255)
+    
+    # 过滤掉尺寸小于随机裁剪输出尺寸的图像
+    def filter(self, imgs):
+        return [img for img in imgs if (img.shape[1] >= self.crop_size[0] and img.shape[2] >= self.crop_size[1])]
+    
+    def __getitem__(self, idx):
+        feature, label = voc_rand_crop(self.features[idx], self.labels[idx], *self.crop_size)
+        return (feature, voc_label_indices(label, self.colormap2label))
+    
+    def __len__(self):
+        return len(self.features)
+    
+
+def load_data_voc(batch_size, crop_size):
+    """加载VOC语义分割数据集"""
+    voc_dir = download_extract('voc2012', os.path.join('VOCdevkit', 'VOC2012'))
+    num_workers = get_dataloader_workers()
+    train_iter = torch.utils.data.DataLoader(VOCSegDataset(True, crop_size, voc_dir), batch_size, shuffle=True, 
+                                             drop_last=True, num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(VOCSegDataset(False, crop_size, voc_dir), batch_size,
+                                            drop_last=True, num_workers=num_workers)
+    return train_iter, test_iter
